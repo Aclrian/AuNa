dc658092ca57135e508656113aca909de72422bd
Aclrian <32142988+Aclrian@users.noreply.github.com>
2025-08-18 19:43:55 +0200

    feat: init package tracinganalysis

---
+++ packages/src/tracinganalysis/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'tracinganalysis'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'notebook>7', 'pandas>2', 'numpy>2', 'bokeh>3.2'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = tracinganalysis.my_node:main'
+        ],
+    },
+)
